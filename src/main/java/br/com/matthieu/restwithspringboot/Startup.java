5d1f46a9ae61c9cb2612f331226e1dfbdbf3bf9d
Matthieu Christian <matthieuceo@gmail.com>
2023-08-30 19:57:05 -0300

    feat: initialize a project

---
+++ src/main/java/br/com/matthieu/restwithspringboot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.matthieu.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
+
+}
